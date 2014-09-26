fd5fb7313f2c387b64650ab3bfb343ebcda49cb4
fetko1977 <feddfriend77@gmail.com>
2014-09-26 10:23:40 +0300

    Added Homework-Exception-Handling.sln

---
+++ Homework-Exception-Handling/Problem-1-Square Root/SquareRoot.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1_Square_Root
+{
+    class SquareRoot
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter an positive integer number: ");
+            string input = Console.ReadLine();
+
+            try
+            {
+                int num = Int32.Parse(input);
+                if (num < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                double result = Math.Sqrt(num);
+                Console.WriteLine(result);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("This number cannot be negative!");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("The value you've entered is not a integer number!");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("The value you've entered is bigger than max value of integer type!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid Operation!");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
---
+++ Homework-Exception-Handling/Problem-2-Enter Numbers/EnterNumbers.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_2_Enter_Numbers
+{
+    class EnterNumbers
+    {
+        static void Main(string[] args)
+        {
+            int start = 0;
+            int end = 100;
+            int[] numbers = new int[10];
+            Console.WriteLine("Please enter ten numbers: ");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                bool isInvalid = false;
+                do {
+                    try
+                    {
+                        numbers[i] = ReadNumbers(start, end);
+                        if (i > 0)
+                        {
+                            if (numbers[i] < numbers[i - 1])
+                            {
+                                throw new ArithmeticException();
+                            }
+                        }
+                        isInvalid = false;
+                    }
+                    catch (ArgumentOutOfRangeException)
+                    {
+                        Console.WriteLine("Number must be in range [{0}...{1}]!", start, end);
+                        Console.WriteLine("Please enter new value: ");
+                        isInvalid = true;
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("The value is not a number!");
+                        Console.WriteLine("Please enter new value: ");
+                        isInvalid = true;
+                    }
+                    catch (ArithmeticException)
+                    {
+                        Console.WriteLine("The current value can't be lower than the previous value!");
+                        Console.WriteLine("Please enter new value: ");
+                        isInvalid = true;
+                    }
+                } while (isInvalid);
+            }
+        }
+
+        public static int ReadNumbers(int start, int end)
+        {
+            string input = Console.ReadLine();
+            int num = Int32.Parse(input);
+            if (num < start || num > end)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            return num;
+        }
+    }
+}
